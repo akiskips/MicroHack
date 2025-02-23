1ef78ebd3330f825249230e8818d252da433b3cb
akiskips <akis100@CPC-admin-1DC4C.>
2025-02-23 22:54:57 +0000

    update tf

--- 03-Azure/01-03-Infrastructure/10_Oracle_on_Azure/resources/environment_setup/terraform/data_guard/kafka_module.tf
+++ 03-Azure/01-03-Infrastructure/10_Oracle_on_Azure/resources/environment_setup/terraform/data_guard/kafka_module.tf
@@ -30,37 +30,37 @@ module "vm_kafka" {
   }
   
 }
-  module "vm_win" {
-  source = "./modules/compute"
+#   module "vm_win" {
+#   source = "./modules/compute"
 
-  resource_group_name = module.common_infrastructure.created_resource_group_name
-  location            = var.location
-  vm_name             = "vm-win-0"
-  public_key          = var.ssh_key
-  sid_username        = "oracle"
-  vm_sku              = var.vm_win_sku
+#   resource_group_name = module.common_infrastructure.created_resource_group_name
+#   location            = var.location
+#   vm_name             = "vm-win-0"
+#   public_key          = var.ssh_key
+#   sid_username        = "oracle"
+#   vm_sku              = var.vm_win_sku
 
-  vm_source_image_reference     = var.vm_win_image_reference
-  aad_system_assigned_identity  = true
-  public_ip_address_resource_id = module.network.db_server_puplic_ip_resources[3].id
+#   vm_source_image_reference     = var.vm_win_image_reference
+#   aad_system_assigned_identity  = true
+#   public_ip_address_resource_id = module.network.db_server_puplic_ip_resources[3].id
 
-  tags                           = module.common_infrastructure.tags
-  db_subnet                      = module.network.db_subnet
+#   tags                           = module.common_infrastructure.tags
+#   db_subnet                      = module.network.db_subnet
 
-  availability_zone = 1
-
-
-  vm_os_disk = {
-    name                   = "osdisk-win"
-    caching                = "ReadWrite"
-    storage_account_type   = "Premium_LRS"
-    disk_encryption_set_id = null
-    disk_size_gb           = 128
-  }
+#   availability_zone = 1
 
 
-  depends_on = [module.network, module.common_infrastructure]
-}
+#   vm_os_disk = {
+#     name                   = "osdisk-win"
+#     caching                = "ReadWrite"
+#     storage_account_type   = "Premium_LRS"
+#     disk_encryption_set_id = null
+#     disk_size_gb           = 128
+#   }
+
+
+#   depends_on = [module.network, module.common_infrastructure]
+# }
 
 
 
